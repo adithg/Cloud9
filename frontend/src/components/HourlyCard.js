d6f3aaa11c105ab0263a80786e0b98f9a8ca6de9
adithg <40368178+adithg@users.noreply.github.com>
2024-04-16 15:43:56 -0500

    Update HourlyCard.js

--- frontend/src/components/HourlyCard.js
+++ frontend/src/components/HourlyCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sunny from '../Icons/SunnyWhite.svg';
 import NightCloudy from '../Icons/NightCloudyWhite.svg';
 import Night from '../Icons/NightWhite.svg';
@@ -9,12 +9,29 @@ const icons = {
   Night: Night,
 };
 
-const HourlyCard = ({ icon, time }) => {
+const getIconBasedOnTime = (time) => {
+  const hours = new Date(time).getHours();
+  if (hours >= 6 && hours < 18) {
+    return 'Sunny';
+  } else if (hours >= 18 && hours < 21) {
+    return 'NightCloudy';
+  } else {
+    return 'Night';
+  }
+};
+
+const HourlyCard = ({ time }) => {
+  const [icon, setIcon] = useState(getIconBasedOnTime(time));
+
+  useEffect(() => {
+    setIcon(getIconBasedOnTime(time));
+  }, [time]);
+
   return (
     <div className="col-span-1 max-w-3xl mx-auto bg-gray-100/[0.1] border-gray-100/[0.3] border-2 rounded-xl flex flex-col justify-center items-center px-6 py-6 mt-8">
       <div className="font-medium text-xl text-gray-100">Now</div>
       <div>
-        <img src={icon} className="h-24 w-24" alt="Weather Icon" />
+        <img src={icons[icon]} className="h-24 w-24" alt="Weather Icon" />
       </div>
       <div className="font-medium text-2xl text-gray-100">{time}</div>
     </div>
