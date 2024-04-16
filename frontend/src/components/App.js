0a04cbac399dcf9e19250c73eecb9d45c90439b0
Antony Sajesh <antonysajesh321@gmail.com>
2024-04-16 00:49:36 -0500

    updatedfrontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Header from './Header';
 import Temperature from './Temperature';
 import HourlyCard from './HourlyCard';
-import Sunny from '../Icons/Sunny.svg';
-import NightCloudy from '../Icons/NightCloudy.svg';
-import Night from '../Icons/Night.svg';
-
+import Sunny from '../Icons/SunnyWhite.svg';
+import NightCloudy from '../Icons/NightCloudyWhite.svg';
+import Night from '../Icons/NightWhite.svg';
 
 const icons = {
-    Sunny: Sunny,
-    NightCloudy: NightCloudy,
-    Night: Night,
-  };
+  Sunny: Sunny,
+  NightCloudy: NightCloudy,
+  Night: Night,
+};
+
 const App = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-gradient-to-b from-blue-600 to-sky-400">
       <Header />
       <Temperature
         temperature={77}
@@ -23,7 +23,9 @@ const App = () => {
         wind={60}
         feelsLike={60}
       />
-      <div className="text-3xl font-medium max-w-5xl mx-auto mt-16">Hourly</div>
+      <div className="text-3xl font-medium max-w-5xl mx-auto mt-16 text-gray-100">
+        Hourly
+      </div>
       <div className="grid grid-cols-6 max-w-5xl mx-auto">
         <HourlyCard icon={icons.Sunny} time="7pm" />
         <HourlyCard icon={icons.NightCloudy} time="7pm" />
@@ -32,6 +34,16 @@ const App = () => {
         <HourlyCard icon={icons.Night} time="7pm" />
         <HourlyCard icon={icons.Night} time="7pm" />
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 };
